55762bddddfcafcbe0f94ad10141cc90c75dfdeb
Colin Rubbert <colinrubbert@gmail.com>
2016-02-19 11:16:44 -0600

    Style acrhive page

--- wp-content/themes/bootstrap2wordpress/archive.php
+++ wp-content/themes/bootstrap2wordpress/archive.php
@@ -9,43 +9,50 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
 		<?php
 		if ( have_posts() ) : ?>
+			<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				<h1>
+					<?php
+						the_archive_title();
+						the_archive_description( '<small class="taxonomy-description">', '</small>' );
+					?>
+				</h1>
+			</section>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<div class="container">
+	      <div class="row" id="primary">
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+	        <main id="content" class="col-sm-8">
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
 
-			endwhile;
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_format() );
 
-			the_posts_navigation();
+						endwhile;
 
-		else :
+						the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+					else :
 
-		endif; ?>
+						get_template_part( 'template-parts/content', 'none' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					endif; ?>
+				</main><!-- main content -->
+
+				<aside class="col-sm-4">
+					<?php get_sidebar(); ?>
+				</aside>
+
+			</div><!-- row #primary -->
+		</div><!-- container -->
 
 <?php
-get_sidebar();
 get_footer();
--- wp-content/themes/bootstrap2wordpress/header.php
+++ wp-content/themes/bootstrap2wordpress/header.php
@@ -22,7 +22,7 @@
     <!-- Font Awesome Icons -->
     <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- Google Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:200,400,700' rel='stylesheet' type='text/css'>
 
 		<?php wp_head(); ?>
 	</head>
